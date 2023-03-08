83442e8e93a70826cf6ca2fa86762b88b4e39322
Greg Trahan <trahan.greg@gmail.com>
2023-03-08 15:00:22 -0600

    Katy da bomb

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,16 @@ const Header = () => {
         <div style={{background: "#FAF9F6"}}>
           <ul style={{listStyleType:"none", display: "flex", margin:"0px", padding:"5px"}}>
             <li className="nav-item">
-              <a className="nav-link active"  href="/" style={{textDecoration: "underline", fontSize: "1.2em", paddingLeft: "2em"}}>Home</a>
+              <a className="nav-link active"  href="/profile" style={{textDecoration: "underline", fontSize: "1.2em", paddingLeft: "2em"}}>Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/projects" style={{textDecoration: "underline", fontSize: "1.2em", paddingLeft: "2em"}}>My Projects</a>
+              <a className="nav-link" href="/profile/projects" style={{textDecoration: "underline", fontSize: "1.2em", paddingLeft: "2em"}}>My Projects</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contact" style={{textDecoration: "underline", fontSize: "1.2em", paddingLeft: "2em"}}>Contact Me</a>
+              <a className="nav-link" href="/profile/contact" style={{textDecoration: "underline", fontSize: "1.2em", paddingLeft: "2em"}}>Contact Me</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/resume" style={{textDecoration: "underline", fontSize: "1.2em", paddingLeft: "2em"}}>Resume</a>
+              <a className="nav-link" href="/profile/resume" style={{textDecoration: "underline", fontSize: "1.2em", paddingLeft: "2em"}}>Resume</a>
             </li>
           </ul>
         </div>
